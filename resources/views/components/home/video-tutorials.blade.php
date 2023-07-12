369978bfe840822f635fac2fd9f507f9c0dad758
Zepp <zeppgoesblack@gmail.com>
2023-07-12 23:31:48 +0500

    video section completed

--- resources/views/components/home/video-tutorials.blade.php
+++ resources/views/components/home/video-tutorials.blade.php
@@ -1,3 +1,37 @@
-<div>
-    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
-</div>
+<!-- Cards section start -->
+	<section id="tutorials" class="dark:bg-slate-800 pt-24 pb-16">
+		<div class="container">
+			<div class="flex flex-wrap -mx-4">
+				<div class="w-full px-4">
+					<div class="text-center mx-auto	mb-[60px] max-w-[510px]">
+						<h2 class="font-bold text-3xl text-dark dark:text-gray-300 mb-4">
+							Video Tutorials
+						</h2>
+						<p class="text-base text-body-color">
+							You learn biba-bubing is strong with the force!
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="flex flex-wrap -mx-4">
+				@foreach($videoTutorials as $video)
+					<x-video-tutorial-item
+						:video-id="$video['videoId']"
+						:title="$video['title']"
+						:description="$video['description']"
+					>
+					</x-video-tutorial-item>
+				@endforeach
+			</div>
+			<div class="flex justify-center">
+				<x-button-link
+					href="https://youtube.com/ProgramWithGio"
+					target="_blank"
+					class="rounded-lg"
+				>
+					View good learning things!
+				</x-button-link>
+			</div>
+		</div>
+	</section>
+<!-- Cards section end -->
